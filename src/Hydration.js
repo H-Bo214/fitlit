a1d38df941161470f1e1bde2eea5c740ddede365
Horacio Borrego <hborrego365@gmail.com>
2020-05-13 19:49:44 -0600

    Add calculateTotalDailyFLOzPerWeek method

--- src/Hydration.js
+++ src/Hydration.js
@@ -8,14 +8,28 @@ class Hydration {
     return this.hydrationData.filter(entry => entry.userID === this.currentUser.id)
   }
   
-  calculateTotalAverageDailyFlOz(id) {
+  calculateTotalAverageDailyFlOz() {
     const currentUserHydrationData = this.returnCurrentUserHydrationData();
     const totalAverageFlOz = currentUserHydrationData.reduce((acc, dailyOzIntake) => {
-     return acc += dailyOzIntake.numOunces
+      return acc += dailyOzIntake.numOunces
     }, 0)
     return Math.round(totalAverageFlOz / currentUserHydrationData.length)
   }
+
+  calculateTotalDailyFlOzPerWeek(date) {
+    const lastSevenDays = []
+    const currentUserHydrationData = this.returnCurrentUserHydrationData();
+    const todaysDate = currentUserHydrationData.find(entry => {
+      entry.date === date 
+    })
+    const latestEntry = currentUserHydrationData.indexOf(todaysDate)
+    for (var i = 1; i < 8; i++) {
+      lastSevenDays.push(currentUserHydrationData[latestEntry + 1])
+    }
+  }
 }
+
+
 
 if (typeof module !== 'undefined') {
   module.exports = Hydration;
