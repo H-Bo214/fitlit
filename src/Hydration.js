dc940142a95d827e433923e42bc4aacfc9ae1d15
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-05-13 18:01:32 -0600

    :crystal_ball: completed hydration constructor

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,3 +1,9 @@
 class Hydration {
-  constructor()
+  constructor(hydrationData, user) {
+    this.id = hydrationData.id;
+    this.date = hydrationData.date;
+    this.ounces = hydrationData.numOunces;
+    this.userHydration(user)
+  }
+
 }
