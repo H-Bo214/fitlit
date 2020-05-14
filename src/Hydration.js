90c41c89ebb1a21b0eb886566ba202e155c917e6
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-05-13 19:09:47 -0600

    :crystal_ball: created & displayed totalAveFlOz on Dom

--- src/Hydration.js
+++ src/Hydration.js
@@ -4,7 +4,23 @@ class Hydration {
     this.currentUser = user;
   }
 
-  calculateCurrentUserTotalFlOz(id) {
+  returnCurrentUserHydrationData() {
     return this.hydrationData.filter(entry => entry.userID === this.currentUser.id)
   }
+  
+  calculateTotalAverageDailyFlOz(id) {
+    const currentUserHydrationData = this.returnCurrentUserHydrationData();
+    console.log('currentUserHydrationData', currentUserHydrationData[0]);
+    
+    const totalAverageFlOz = currentUserHydrationData.reduce((acc, dailyOzIntake) => {
+      acc += dailyOzIntake.numOunces
+      console.log('acc', acc);
+      return acc
+    }, 0)
+    return Math.round(totalAverageFlOz / currentUserHydrationData.length)
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = Hydration;
 }
