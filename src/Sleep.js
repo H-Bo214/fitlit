3850903afb3feda4a6d537c124327ddd5de4f701
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-05-14 18:19:17 -0600

    :crystal_ball: method link user id to data

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,5 +1,11 @@
 class Sleep {
   constructor(sleepData, user) {
-    
+    this.sleepData = sleepData;
+    this.currentUser = user;
   }
+
+  returnCurrentUserSleepData() {
+    return this.sleepData.filter(entry => entry.userID === this.currentUser.id)
+  }
+  
 }
